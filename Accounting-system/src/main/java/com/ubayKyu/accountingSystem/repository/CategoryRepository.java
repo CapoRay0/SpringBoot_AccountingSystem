94c136a86482b6a1cf6ebdc608774666d8fe583e
Ray <Ray@192.168.0.11>
2022-01-10 21:15:46 +0800

    AccRepo AmountSumThisMonth Update

--- Accounting-system/src/main/java/com/ubayKyu/accountingSystem/repository/CategoryRepository.java
+++ Accounting-system/src/main/java/com/ubayKyu/accountingSystem/repository/CategoryRepository.java
@@ -17,41 +17,41 @@ public interface CategoryRepository extends JpaRepository<Category, String>{
 	
 	// 取得登入者的分類資訊(Interface)
 	@Query(value = "SELECT C.[categoryid]"
-				+ " 	,C.[body]"
-				+ "    	,C.[caption]"
-				+ "    	,FORMAT(C.[create_date], 'yyyy/MM/dd') AS [create_date]"
-				+ "    	,C.[userid]"
-				+ "    	,COUNT(A.[categoryid]) [count]"
-				+ "	FROM [category] AS C"
-				+ "	LEFT JOIN [accounting_note] AS A ON A.[categoryid] = C.[categoryid]"
-				+ " WHERE C.[userid] =:userid"
-				+ " GROUP BY C.[categoryid], C.[body], C.[caption], C.[create_date], C.[userid]"
-				+ " ORDER BY [count] DESC, [create_date] DESC"
-				, nativeQuery = true)
+			+ "    ,C.[body]"
+			+ "    ,C.[caption]"
+			+ "    ,FORMAT(C.[create_date], 'yyyy/MM/dd') AS [create_date]"
+			+ "    ,C.[userid]"
+			+ "    ,COUNT(A.[categoryid]) [count]"
+			+ " FROM [category] AS C"
+			+ " LEFT JOIN [accounting_note] AS A ON A.[categoryid] = C.[categoryid]"
+			+ " WHERE C.[userid] =:userid"
+			+ " GROUP BY C.[categoryid], C.[body], C.[caption], C.[create_date], C.[userid]"
+			+ " ORDER BY [count] DESC, [create_date] DESC"
+			, nativeQuery = true)
 	 List<CategoryInterface> GetCategoryInterfaceListByUserID(@Param("userid") String userid);
 	
 	// 取得該分類資訊旗下的流水帳數量(count) >> 若為0才可刪除分類
 	@Query(value = "SELECT COUNT(A.categoryid) [count]"
-				+ " FROM category AS C"
-				+ " LEFT JOIN [accounting_note] AS A ON A.[categoryid] = C.[categoryid]"
-				+ " WHERE C.[categoryid] =:categoryid"
-				+ " GROUP BY C.[categoryid]"
-				, nativeQuery = true)
+			+ " FROM category AS C"
+			+ " LEFT JOIN [accounting_note] AS A ON A.[categoryid] = C.[categoryid]"
+			+ " WHERE C.[categoryid] =:categoryid"
+			+ " GROUP BY C.[categoryid]"
+			, nativeQuery = true)
 	Integer GetCountByCategoryIDForDel(@Param("categoryid") String categoryid);
 	
 	// 檢查是否有重複的標題
 	@Query(value = "SELECT COUNT(*) [count]"
-				+ " FROM [category]"
-				+ " WHERE [userid] =:userid AND [caption] =:caption"
-				, nativeQuery = true)
+			+ " FROM [category]"
+			+ " WHERE [userid] =:userid AND [caption] =:caption"
+			, nativeQuery = true)
 	int IsCategoryCaptionExistFindByCaptionAndUserID(@Param("userid") String userid, @Param("caption") String caption);
 	
 	/*------------------------AccountingDetail.html--------------------------*/
 	
 	// 顯示出AccountingDetail中的分類下拉選單
 	@Query(value = "SELECT *"
-				+ " FROM [category]"
-				+ " WHERE [userid] =:userid"
-				, nativeQuery = true)
+			+ " FROM [category]"
+			+ " WHERE [userid] =:userid"
+			, nativeQuery = true)
 	List<Category> GetCategoryByUserID(@Param("userid") String userid);
 }
--- Accounting-system/src/main/java/com/ubayKyu/accountingSystem/repository/UserInfoRepository.java
+++ Accounting-system/src/main/java/com/ubayKyu/accountingSystem/repository/UserInfoRepository.java
@@ -18,70 +18,70 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, String>{
 	
 	// 進行登入驗證，同時查詢登入者的個人資訊
 	@Query(value = "SELECT [userid]"
-				+ "		,[account]"
-				+ "    	,[create_date]"
-				+ "    	,[email]"
-				+ "    	,[name]"
-				+ "    	,[pwd]"
-				+ "    	,[user_level]"
-				+ "    	,[edit_date]"
-				+ "	FROM [user_info]"
-				+ " WHERE [account]=:account AND [pwd]=:pwd"
-				, nativeQuery = true)
+			+ "    ,[account]"
+			+ "    ,[create_date]"
+			+ "    ,[email]"
+			+ "    ,[name]"
+			+ "    ,[pwd]"
+			+ "    ,[user_level]"
+			+ "    ,[edit_date]"
+			+ " FROM [user_info]"
+			+ " WHERE [account]=:account AND [pwd]=:pwd"
+			, nativeQuery = true)
 	 UserInfo GetUserInfoForLogin(@Param("account") String account, @Param("pwd") String pwd);
 	
 	/*----------------------------UserList.html------------------------------*/
 	
 	//取得會員資訊(Interface)
 	@Query(value = "SELECT [userid]"
-				+ " 	,[account]"
-				+ "    	,FORMAT([create_date], 'yyyy/MM/dd hh:mm') AS [create_date]"
-				+ "    	,[email]"
-				+ "    	,[name]"
-				+ "    	,[user_level]"
-				+ "	FROM [user_info]"
-				+ "	ORDER BY [create_date] DESC"
-				, nativeQuery = true)
+			+ "    ,[account]"
+			+ "    ,FORMAT([create_date], 'yyyy/MM/dd hh:mm') AS [create_date]"
+			+ "    ,[email]"
+			+ "    ,[name]"
+			+ "    ,[user_level]"
+			+ " FROM [user_info]"
+			+ " ORDER BY [create_date] DESC"
+			, nativeQuery = true)
 	List<UserInfoInterface> GetUserInfoInterface();
 	
 	//若SQL不回傳則須加上 @Modifying
 	//刪除會員、流水帳及分類資訊
 	@Query(value = "DELETE FROM [user_info]"
-				+ "	WHERE [user_info].[userid] =:userid"
-				+ " DELETE FROM [accounting_note]"
-				+ "	WHERE [accounting_note].[userid] =:userid"
-				+ "	DELETE FROM [category]"
-				+ "	WHERE [category].[userid] =:userid"
-				+ "	SELECT COUNT(*) FROM [user_info]"
-				, nativeQuery = true)
+			+ " WHERE [user_info].[userid] =:userid"
+			+ " DELETE FROM [accounting_note]"
+			+ " WHERE [accounting_note].[userid] =:userid"
+			+ " DELETE FROM [category]"
+			+ " WHERE [category].[userid] =:userid"
+			+ " SELECT COUNT(*) FROM [user_info]"
+			, nativeQuery = true)
 	Integer DeleteUserInfoAccountingNoteAndCategoryByUserID(@Param("userid") String userid);
 	
 	/*---------------------------UserDetail.html-----------------------------*/
 	
 	//取得個人資訊以進行編輯
 	@Query(value = "SELECT [userid]"
-				+ " 	,[account]"
-				+ " 	,FORMAT([create_date], 'yyyy/MM/dd hh:mm:ss') AS [create_date]"
-				+ " 	,[email]"
-				+ " 	,[name]"
-				+ " 	,[user_level]"
-				+ " 	,FORMAT([edit_date], 'yyyy/MM/dd hh:mm:ss') AS [edit_date]"
-				+ " FROM [user_info]"
-				+ " WHERE [userid] =:userid"
-				, nativeQuery = true)
+			+ "    ,[account]"
+			+ "    ,FORMAT([create_date], 'yyyy/MM/dd hh:mm:ss') AS [create_date]"
+			+ "    ,[email]"
+			+ "    ,[name]"
+			+ "    ,[user_level]"
+			+ "    ,FORMAT([edit_date], 'yyyy/MM/dd hh:mm:ss') AS [edit_date]"
+			+ " FROM [user_info]"
+			+ " WHERE [userid] =:userid"
+			, nativeQuery = true)
 	Optional<UserInfoInterface> GetUserInfoInterfaceByUserID(@Param("userid") String userid);
 	
 	//檢查帳號是否重複
 	@Query(value = "SELECT COUNT(*)"
-				+ " FROM [user_info]"
-				+ " WHERE [account] =:account"
-				, nativeQuery = true)
+			+ " FROM [user_info]"
+			+ " WHERE [account] =:account"
+			, nativeQuery = true)
 	int GetUserAccountByAccount(@Param("account") String account);
 	
 	//取得當前管理員人數
 	@Query(value = "SELECT COUNT(*)"
-				+ " FROM [user_info]"
-				+ " WHERE [user_level] = 0"
-				, nativeQuery = true)
+			+ " FROM [user_info]"
+			+ " WHERE [user_level] = 0"
+			, nativeQuery = true)
 	int GetAdminUserCount();
 }
